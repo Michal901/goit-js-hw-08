20ffa69d79fae6911040aa482ff0f39c90bdfbdc
Michal901 <momot1122@wp.pl>
2024-02-02 10:44:59 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createItems = images =>
+  images
+    .map(
+      image =>
+        `<li  class="gallery__item">
+         <a   "gallery__link" href="${image.original}">
+         <img
+            class="gallery__image"
+            src="${image.preview}"
+            alt="${image.description}"
+            />
+         </a>
+         </li>`
+    )
+    .join('');
+
+const addItems = createItems(galleryItems);
+
+gallery.innerHTML = addItems;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionType: 'attr',
+  captionDelay: 250,
+});
