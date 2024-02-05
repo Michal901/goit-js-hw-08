ba76d0ae17ccff3e33f69f221732af66b65bb5e6
Michal901 <momot1122@wp.pl>
2024-02-05 15:16:11 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector("input[name='email']");
+const message = form.querySelector("textarea[name='message']");
+const storage = 'feedback-form-state';
+
+const saveToStorage = throttle(() => {
+  const state = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  localStorage.setItem(storage, JSON.stringify(state));
+}, 500);
+
+form.addEventListener('input', saveToStorage);
+
+const loadFromStorage = () => {
+  const stored = localStorage.getItem(storage);
+  if (stored) {
+    try {
+      const state = JSON.parse(stored);
+      email.value = state.email;
+      message.value = state.message;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+};
+
+loadFromStorage();
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem(storage);
+  form.reset();
+});
